071bad786613e3ef8fc886f6e8b1df821a993236
abhiroop.nandiray@mindtree.com <abhiroop.nandiray@mindtree.com>
2018-09-08 19:57:25 +0530

    Added checks

--- src/com/practice/main/MinCharToReplace.java
+++ src/com/practice/main/MinCharToReplace.java
@@ -3,12 +3,10 @@ package com.practice.main;
 public class MinCharToReplace {
     public static void main(String[] args) {
         String str1 = "Intellect";
-        String str2 = "tell"/*"pigs"*/;
+        String str2 = /*"tell"*//*"pigs"*/ "Intelle";
         FindMatcher matcher = new FindMatcher();
-       /* boolean matchFound = */matcher.findMatch(str1, str2);
-        //System.out.println("Match found: " + matchFound);
+        matcher.findMatch(str1, str2);
     }
-
 
 
 }
